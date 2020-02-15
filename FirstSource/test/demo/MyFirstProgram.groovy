3420267fc46b0c65048c9848be8739d76dcf4972
Rocky Bhai <rocky@sabkabaap.com>
2020-02-15 05:26:28 +0000

    15th Feb - 1st Commit

---
+++ FirstSource/test/demo/MyFirstProgram.groovy
@@ -0,0 +1,13 @@
+package test.demo
+
+//First program in Groovy
+
+class MyFirstProgram {
+
+	static void main(args) {
+		println "Hello World!"
+		println "Welcome to Groovy!"
+		println "This is my first Groovy program!" 
+
+	}
+}
