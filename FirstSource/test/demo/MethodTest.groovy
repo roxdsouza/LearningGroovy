e85d74b57b7efdb404995b395258a11c85f33853
roxdsouza <41199121+roxdsouza@users.noreply.github.com>
2020-02-16 19:29:01 +0800

    16th Feb - 1st Commit
    16th Feb - 1st Commit

---
+++ FirstSource/test/demo/MethodTest.groovy
@@ -0,0 +1,15 @@
+class MethodTest {
+    
+    static void main(args) {
+        // Need to create an object of MethodTest class
+        
+        MethodTest m1 = new MethodTest()
+        m1.myMethod()
+        println "Done."
+    }
+    
+    
+    def myMethod() {
+        println "I am inside my method"
+    }
+}
